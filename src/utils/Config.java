b797d429e33497dde301cdce43cd645b8cf195e2
Matheus <matheusfelipeferreiramartins@gmail.com>
2021-03-06 17:40:51 -0300

    fix: no src

--- src/utils/Config.java
+++ src/utils/Config.java
@@ -8,9 +8,9 @@ public class Config {
 
     public static final String REGISTRY_NAME = "Election";
     public static final int HOST = 1888;
-    public static final String FILE_NAME = "./src/files/senadores.csv";
-    public static final String FILE_CACHE_NAME = "./src/files/senadoresCache.txt";
-    public static final String FILE_VOTES_CACHE = "./src/files/votosCache.txt";
+    public static final String FILE_NAME = "./files/senadores.csv";
+    public static final String FILE_CACHE_NAME = "./files/senadoresCache.txt";
+    public static final String FILE_VOTES_CACHE = "./files/votosCache.txt";
     public static final int MAX_TRIES = 5;
     public static final int TRIES_INTERVAL = 6000;
 }
